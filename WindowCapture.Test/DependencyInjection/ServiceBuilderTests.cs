ac1e9eba8a7af5acffaeb1cd13da74d810f7ab61
naminodarie <gengesa@gmail.com>
2021-04-02 02:09:30 +0900

    DIのテスト

---
+++ WindowCapture.Test/DependencyInjection/ServiceBuilderTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Kzrnm.WindowCapture.DependencyInjection
+{
+    public class ServiceBuilderTests
+    {
+        public static TheoryData EnumerateViewModels()
+        {
+            var theoryData = new TheoryData<Type>();
+            foreach (var t in typeof(ServiceBuilder).Assembly.GetTypes())
+                if (t.Namespace.EndsWith(".ViewModels"))
+                    theoryData.Add(t);
+            return theoryData;
+        }
+        [Theory]
+        [MemberData(nameof(EnumerateViewModels))]
+        public void ViewModels(Type type)
+        {
+            var ioc = new Ioc();
+            ioc.ConfigureServices(ServiceBuilder.Default.BuildServiceProvider());
+            ioc.GetService(type).Should().BeOfType(type);
+        }
+    }
+}
--- WindowCapture/DependencyInjection/IocBehavior.cs
+++ WindowCapture/DependencyInjection/IocBehavior.cs
@@ -7,6 +7,8 @@ namespace Kzrnm.WindowCapture.DependencyInjection
 {
     public static class IocBehavior
     {
+        internal static Ioc Ioc { set; get; } = Ioc.Default;
+
         public static Type GetAutoViewModel(DependencyObject obj) => (Type)obj.GetValue(AutoViewModelProperty);
         public static void SetAutoViewModel(DependencyObject obj, Type value) => obj.SetValue(AutoViewModelProperty, value);
         public static readonly DependencyProperty AutoViewModelProperty =
@@ -14,20 +16,16 @@ namespace Kzrnm.WindowCapture.DependencyInjection
                 "AutoViewModel",
                 typeof(Type),
                 typeof(IocBehavior),
-                new FrameworkPropertyMetadata(null, 
-                    FrameworkPropertyMetadataOptions.NotDataBindable, 
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.NotDataBindable,
                     AutoViewModelChanged));
 
         private static void AutoViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (DesignerProperties.GetIsInDesignMode(d))
                 return;
-            if (d is not FrameworkElement elm) return;
-            if (e.NewValue is Type type)
-            {
-                elm.DataContext = Ioc.Default.GetService(type);
-            }
-            else elm.DataContext = null;
+            if (d is FrameworkElement elm && e.NewValue is Type type)
+                elm.DataContext = Ioc.GetService(type);
         }
     }
 }
